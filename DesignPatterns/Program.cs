b5a3aa06ace3e77cd63d9e3dbfe6275f92776eb7
Paweł Mrówczyński <pawel.mrowczynski@outlook.com>
2016-10-09 19:02:48 +0200

    Testing github branch protection.

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -10,7 +10,7 @@ namespace DesignPatterns
     {
         static void Main(string[] args)
         {
-            string test = "asd5";
+            string test = "asdef5";
             Console.WriteLine(test);
         }
     }
