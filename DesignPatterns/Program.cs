71b8e362feea411d0b0b708dc92ecc9d7548ffb9
Paweł Mrówczyński <pawel.mrowczynski@outlook.com>
2016-10-09 18:57:21 +0200

    Create develop branch

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -10,7 +10,7 @@ namespace DesignPatterns
     {
         static void Main(string[] args)
         {
-            string test = "asd";
+            string test = "asd5";
             Console.WriteLine(test);
         }
     }
